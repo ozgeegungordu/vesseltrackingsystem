fc05d37352b3d07d287250927bb9ab344ea37515
Özge GÜNGÖRDÜ <139129999+ozgeegungordu@users.noreply.github.com>
2024-11-20 21:54:47 +0300

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VesselTrackingSystem.Models;
+
+namespace VesselTrackingSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        private LimakPortContext _context;
+
+        public AccountController(LimakPortContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult> Login(Users model)
+        {
+            if (ModelState.IsValid)
+            {
+                string PasswordHash = CreateMD5(model.Password);
+                var user = await _context.Users.SingleOrDefaultAsync(u => u.Username == model.Username && u.Password == PasswordHash);
+
+                if (user != null)
+                {
+                    return RedirectToAction("Map", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid credentials");
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+
+        public static string CreateMD5(string input)
+        {
+
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                return Convert.ToHexString(hashBytes); 
+            }
+        }
+    }
+}
+
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VesselTrackingSystem.Models;
+
+namespace VesselTrackingSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private LimakPortContext _context;
+
+        public HomeController(LimakPortContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Map()
+        {
+            //var serviceRouteLocations = _context.Services.ToList();
+            //var viewModel = new Services
+            //{
+            //    ServiceList = new SelectList(services, "Id", "Name"),
+            //};
+
+            return View(/*viewModel*/);
+
+        }
+
+        [HttpPost]
+        //public IActionResult Map(Services viewModel)
+        //{
+        //    int selectedServiceId = viewModel.SelectedServiceId;
+
+        //    return View(viewModel);
+        //}
+
+
+        public IActionResult AddVessel()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddVessel(Vessels model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Vessels.Add(model);
+                _context.SaveChanges();
+            }
+            return View();
+        }
+
+        public IActionResult AddService()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddService(Services model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Services.Add(model);
+                _context.SaveChanges();
+            }
+
+            return View();
+        }
+
+        public IActionResult AddPort()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult AddPort(Ports model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Ports.Add(model);
+                _context.SaveChanges();
+            }
+
+            return View();
+        }
+
+        public IActionResult VesselList()
+        {
+            var vessels = _context.Vessels.ToList();
+
+            return View(vessels);
+
+        }
+
+        public IActionResult PortList()
+        {
+            var ports = _context.Ports.ToList();
+
+            return View(ports);
+        }
+
+        public IActionResult ServiceList()
+        {
+            var services = _context.Services.ToList();
+
+            return View(services);
+        }
+    }
+}
